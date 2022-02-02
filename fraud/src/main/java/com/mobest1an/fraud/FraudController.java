d6f4f35a331203709994f349b1c7fa21855b1af0
mobest1an <erikk202@gmail.com>
2022-02-02 21:56:57 +0300

    Beta RestTemplate + Everything for Service Discovery without communication

--- fraud/src/main/java/com/mobest1an/fraud/FraudController.java
+++ fraud/src/main/java/com/mobest1an/fraud/FraudController.java
@@ -1,21 +1,21 @@
 package com.mobest1an.fraud;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
 
-    @PostMapping("/{customerId}")
+    @GetMapping("/{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
